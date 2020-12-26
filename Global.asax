17e2ce0230ecdb9d37647fdb86d6307f9478eef8
27vestnikova <75781562+27vestnikova@users.noreply.github.com>
2020-12-26 23:25:38 +0300

    Проект asp.net mvc
    Обязательно: Создать веб-приложение на базе MVC (разделение кода на модель, представление и контроллер). Несколько моделей со связями (например, содержат ссылки друг на друга).
    Желательно: Помимо MVC-контроллеров - 1-2 AP-контроллера, которые предоставляют информацию на машиночитаемом уровне для потенциальной интеграции с другими серверами. И остальное, из того, что проходили. Или, что изучили самостоятельно.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PolinaVestnikova.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PolinaVestnikova.Startup))]
+namespace PolinaVestnikova
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
